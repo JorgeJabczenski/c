dfa2e25c4459a25180bae0a2d51e95ecd24ba857
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-08-28 21:31:57 -0300

    insertionsort

---
+++ Sorts/Insertion_Sort/insertion_sort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define  N        10
+#define  MAX_RAND 50
+
+void gerar_vetor(int v[], int t);
+void organizar_vetor(int v[], int t);
+void imprimir_vetor(int v[], int t);
+
+
+int main(){
+
+    int vetor[N];
+
+    // Gera uma semente aleatória
+    srand(time(NULL));
+
+    // Gerar vetor de aleatórios
+    gerar_vetor(vetor, N);
+    printf("Vetor desordenado: ");
+    imprimir_vetor(vetor, N);
+
+    // Organizar com Bubble Sort
+    organizar_vetor(vetor, N);
+    printf("Vetor ordenado:    ");
+    imprimir_vetor(vetor, N);
+
+    return 0;
+}
+
+void gerar_vetor(int v[], int t){
+    for (int i = 0; i < t; i++){
+        v[i] = (rand() % MAX_RAND) + 1;
+    }
+}
+
+void organizar_vetor(int v[], int t){
+    for (int i = 1; i < t; i++)
+    {
+        int key = v[i];
+        int j = i-1;
+        while(key < v[j] && j >= 0)
+        {
+            v[j+1] = v[j];
+            j--;
+            
+        }
+        v[j+1] = key;
+        imprimir_vetor(v,t);
+    }
+}
+
+void imprimir_vetor(int v[], int t){
+    for (int i = 0; i < t; i++)
+        printf(" %d", v[i]);
+    printf("\n");
+}
