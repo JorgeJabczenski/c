228cd058a4dda1df00165abd8354e557dc7d743b
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-09-27 21:35:32 -0300

    certo???

--- Recursão/Lista-Raul/ex1/pascalcerto.c
+++ Recursão/Lista-Raul/ex1/pascalcerto.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 
 
-int pascal(){
-    if (l == 0 || c == 0)
+int pascal(int l, int c){
+
+    if (l == 0 || c == 0 || l == c)
         return 1;
 
-    return (pascal);
-    
+    return ( pascal(l-1,c) + pascal(l-1,c-1) );
 }
 
 
 int main(){
 
-    int n, termo;
-
-
+    int n, termo, soma = 0, contador = 0, l, c;
     
+    scanf("%d",&n);
+
+    while(n > soma){
+        soma += contador;
+        contador++;
+    }
+
+    l = contador - 2;
+    c = l - (soma - n);
+
+    termo = pascal(l,c);
+
+    printf("O termo %d é %d\n", n, termo);
+
     return 0;
 }
