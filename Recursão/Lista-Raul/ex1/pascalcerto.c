3421dd55dcb62c2abb498db4145f01ace94fc722
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-09-27 21:07:47 -0300

    lista recursao

---
+++ Recursão/Lista-Raul/ex1/pascalcerto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int pascal(){
+    if (l == 0 || c == 0)
+        return 1;
+
+    return (pascal);
+    
+}
+
+
+int main(){
+
+    int n, termo;
+
+
+    
+    return 0;
+}
---
+++ Recursão/Lista-Raul/ex3/binary-heap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX 17
+
+int binaryheap(){
+
+
+
+
+}
+
+
+
+int main(){
+
+    int vetor[MAX],x;
+    int i;
+
+    /*Para começar em 1*/
+    for (i = 1; i < MAX; i++)
+        vetor[i] = i;
+
+    
+
+
+
+
+
+    return 0;
+}
