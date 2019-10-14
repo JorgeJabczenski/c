c4c9b81214f2d5159a9f19ab746ed785a10a9f7b
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-10-14 07:47:48 -0300

    adicionado os nomes de funções

--- Prog1/Jogo-Da-Vida/jogodavida.c
+++ Prog1/Jogo-Da-Vida/jogodavida.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <curses.h>
 
+int **geracaoAntiga, **geracaoNova;
 
+/* Tentar implementar com loop around | (x + i + linhas) % linhas | (y + j + colunas) % colunas */
+int conta_vizinhos(){
+
+}
+
+void copia_matrizes(){
+
+}
+
+void aloca_matrizes(){
+
+}
 
 
 int main(){
 
 	initscr();
-	
+    clear();
+
 	printw("hello");
 	refresh();
 
@@ -16,8 +30,8 @@ int main(){
 	int i,j;
 
 	for (i = 0; i < 20; i++)
-		for (j = 0; j < 40; j++)
-			mvprintw(j,i,"1");
+		for (j = 0; j < 20; j++)
+			mvprintw(j,i,"~");
 
 	getch();
 
